61e22e981047a80f26da3a96b91ae833bb711908
Luciano Delucchi <luciano.delucchi@gmail.com>
2014-02-03 17:19:36 +0100

    Rename folders

---
+++ Convertidor/Model/Image.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Luciano
+ * Date: 29/01/2014
+ * Time: 12:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Convertidor.Model
+{
+	/// <summary>
+	/// Description of Image.
+	/// </summary>
+	public class Image
+	{
+		public Image()
+		{
+		}
+	}
+}
