8d472b7109f787d7235d69574585f5c8e285467d
Luciano Delucchi <luciano.delucchi@gmail.com>
2014-02-03 17:23:34 +0100

    close #4 Model

---
+++ Convertidor/Models/Image.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Convertidor.Models
+{
+	/// <summary>
+	/// Description of Image.
+	/// </summary>
+	public class Image
+	{
+        #region Creation
+
+        public static Image CreateNewImage()
+        {
+            return new Image();
+        }
+
+        public static Image CreateImage(string filename, string path, string saveAs)
+        {
+            return new Image
+            {
+                Path = path,
+                SaveAs = saveAs,
+                FileName = filename
+            };
+        }
+
+        protected Image()
+        {
+        }
+
+        #endregion // Creation
+	
+     	#region Properties
+
+     	/// <summary>
+        /// Gets/sets the filename of the image.
+        /// </summary>
+        public string FileName { get; set; }	
+     	
+        /// <summary>
+        /// Gets/sets the full path of the image.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets/sets the full path of where to save the converted image.
+        /// </summary>
+        public string SaveAs { get; set; }
+        
+        #endregion // Properties
+
+	}
+}
